01fb2f90114d05df976ded88570c9e8bae9e46d1
Vlad <rog56rus@mail.ru>
2024-09-07 22:49:44 +0500

    fix bugs

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,8 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Group;
-import javafx.scene.text.Text;
+import edu.java.lab2.DogShowAdmin;
 
-public class Main extends Application{
-
+public class Main {
     public static void main(String[] args) {
-
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-
-        // установка надписи
-        Text text = new Text("Hello METANIT.COM!");
-        text.setLayoutY(80);    // установка положения надписи по оси Y
-        text.setLayoutX(80);   // установка положения надписи по оси X
-
-        Group group = new Group(text);
-
-        Scene scene = new Scene(group);
-        stage.setScene(scene);
-        stage.setTitle("JavaFX Application");
-        stage.setWidth(300);
-        stage.setHeight(250);
-        stage.show();
+        DogShowAdmin dogShowAdmin = new DogShowAdmin();
+        dogShowAdmin.show();
     }
 }
